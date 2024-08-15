3fb93eba40e47ade7c2b77ca7caade9acf50ed36
Prem Desai <121574508+PREMDESAI@users.noreply.github.com>
2024-08-16 00:41:57 +0530

    Add files via upload

---
+++ uber clone/components/Payment.tsx
@@ -0,0 +1,155 @@
+import CustomButton from "@/components/CustomButton";
+import { useStripe } from "@stripe/stripe-react-native";
+import { useState } from "react";
+import { Alert, Image, Text, View } from "react-native";
+import { fetchAPI } from "@/lib/fetch";
+import { PaymentProps } from "@/types/type";
+import { useLocationStore } from "@/store";
+import { useAuth } from "@clerk/clerk-expo";
+import { ReactNativeModal } from "react-native-modal";
+import { images } from "@/constants";
+import { router } from "expo-router";
+
+const Payment = ({
+  fullName,
+  email,
+  amount,
+  driverId,
+  rideTime,
+}: PaymentProps) => {
+  const { initPaymentSheet, presentPaymentSheet } = useStripe();
+  const { userId } = useAuth();
+  const [success, setSuccess] = useState(false);
+  const {
+    userAddress,
+    userLongitude,
+    userLatitude,
+    destinationLatitude,
+    destinationAddress,
+    destinationLongitude,
+  } = useLocationStore();
+
+  const initializePaymentSheet = async () => {
+    const { error } = await initPaymentSheet({
+      merchantDisplayName: "Ryde Inc.",
+      intentConfiguration: {
+        mode: {
+          amount: parseInt(amount) * 100,
+          currencyCode: "USD",
+        },
+        confirmHandler: async (paymentMethod, _, intentCreationCallback) => {
+          const { paymentIntent, customer } = await fetchAPI(
+            "/(api)/(stripe)/create",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                name: fullName || email.split("@")[0],
+                email: email,
+                amount: amount,
+                paymentMethodId: paymentMethod.id,
+              }),
+            },
+          );
+
+          if (paymentIntent.client_secret) {
+            const { result } = await fetchAPI("/(api)/(stripe)/pay", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                payment_method_id: paymentMethod.id,
+                payment_intent_id: paymentIntent.id,
+                customer_id: customer,
+              }),
+            });
+
+            if (result.client_secret) {
+              await fetchAPI("/(api)/ride/create", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                  origin_address: userAddress,
+                  destination_address: destinationAddress,
+                  origin_latitude: userLatitude,
+                  origin_longitude: userLongitude,
+                  destination_latitude: destinationLatitude,
+                  destination_longitude: destinationLongitude,
+                  ride_time: rideTime.toFixed(0),
+                  fare_price: parseInt(amount) * 100,
+                  payment_status: "paid",
+                  driver_id: driverId,
+                  user_id: userId,
+                }),
+              });
+
+              intentCreationCallback({
+                clientSecret: result.client_secret,
+              });
+            }
+          }
+        },
+      },
+      returnURL: 'myapp"//book-ride',
+    });
+    if (error) {
+      console.log(error);
+    }
+  };
+
+  const openPaymentSheet = async () => {
+    await initializePaymentSheet();
+
+    const { error } = await presentPaymentSheet();
+
+    if (error) {
+      Alert.alert(`Error code: ${error.code}`, error.message);
+    } else {
+      setSuccess(true);
+    }
+  };
+
+  return (
+    <>
+      <CustomButton
+        title="Confirm Ride"
+        className="my-10"
+        onPress={openPaymentSheet}
+      />
+
+      <ReactNativeModal
+        isVisible={success}
+        onBackdropPress={() => setSuccess(false)}
+      >
+        <View className="flex flex-col items-center justify-center bg-white p-7 rounded-2xl">
+          <Image source={images.check} className="w-28 h-28 mt-5" />
+
+          <Text className="text-2xl text-center font-JakartaBold mt-5">
+            Ride booked!
+          </Text>
+
+          <Text className="text-md text-general-200 font-JakartaMedium text-center mt-3">
+            Thank you for your booking. Your reservation has been placed. Please
+            proceed with your trip!
+          </Text>
+
+          <CustomButton
+            title="Back Home"
+            onPress={() => {
+              setSuccess(false);
+              router.push("/(root)/(tabs)/home");
+            }}
+            className="mt-5"
+          />
+        </View>
+      </ReactNativeModal>
+    </>
+  );
+};
+
+export default Payment;
---
+++ uber clone/components/RideLayout.tsx
@@ -0,0 +1,56 @@
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { router } from "expo-router";
+import { icons } from "@/constants";
+import Map from "@/components/Map";
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
+import { useRef } from "react";
+
+const RideLayout = ({
+  title,
+  children,
+  snapPoints,
+}: {
+  title: string;
+  children: React.ReactNode;
+  snapPoints?: string[];
+}) => {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  return (
+    <GestureHandlerRootView>
+      <View className="flex-1 bg-white">
+        <View className="flex flex-col h-screen bg-blue-500">
+          <View className="flex flex-row absolute z-10 top-16 items-center justify-start px-5">
+            <TouchableOpacity onPress={() => router.back()}>
+              <View className="w-10 h-10 bg-white rounded-full items-center justify-center">
+                <Image
+                  source={icons.backArrow}
+                  resizeMode="contain"
+                  className="w-6 h-6"
+                />
+              </View>
+            </TouchableOpacity>
+            <Text className="text-xl font-JakartaSemiBold ml-5">
+              {title || "Go Back"}
+            </Text>
+          </View>
+
+          <Map />
+        </View>
+
+        <BottomSheet
+          ref={bottomSheetRef}
+          snapPoints={snapPoints || ["40%", "85%"]}
+          index={0}
+        >
+          <BottomSheetView style={{ flex: 1, padding: 20 }}>
+            {children}
+          </BottomSheetView>
+        </BottomSheet>
+      </View>
+    </GestureHandlerRootView>
+  );
+};
+
+export default RideLayout;
