3ba24bd655ac5439e2918036ca26415551562fb4
Prem Desai <121574508+PREMDESAI@users.noreply.github.com>
2024-08-16 00:40:29 +0530

    Add files via upload

---
+++ uber clone/app/(api)/(stripe)/create+api.ts
@@ -0,0 +1,55 @@
+import { Stripe } from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { name, email, amount } = body;
+
+  if (!name || !email || !amount) {
+    return new Response(
+      JSON.stringify({
+        error: "Please enter a valid email address",
+        status: 400,
+      }),
+    );
+  }
+
+  let customer;
+
+  const existingCustomer = await stripe.customers.list({ email });
+
+  if (existingCustomer.data.length > 0) {
+    customer = existingCustomer.data[0];
+  } else {
+    const newCustomer = await stripe.customers.create({
+      name,
+      email,
+    });
+
+    customer = newCustomer;
+  }
+
+  const ephemeralKey = await stripe.ephemeralKeys.create(
+    { customer: customer.id },
+    { apiVersion: "2020-08-27" },
+  );
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: parseInt(amount) * 100,
+    currency: "usd",
+    customer: customer.id,
+    automatic_payment_methods: {
+      enabled: true,
+      allow_redirects: "never",
+    },
+  });
+
+  return new Response(
+    JSON.stringify({
+      paymentIntent: paymentIntent,
+      ephemeralKey: ephemeralKey,
+      customer: customer.id,
+    }),
+  );
+}
---
+++ uber clone/app/(root)/confirm-ride.tsx
@@ -0,0 +1,35 @@
+import { FlatList, Text, View } from "react-native";
+import RideLayout from "@/components/RideLayout";
+import DriverCard from "@/components/DriverCard";
+import CustomButton from "@/components/CustomButton";
+import { router } from "expo-router";
+import { useDriverStore } from "@/store";
+
+const ConfirmRide = () => {
+  const { drivers, selectedDriver, setSelectedDriver } = useDriverStore();
+
+  return (
+    <RideLayout title="Choose a Driver" snapPoints={["65%", "85%"]}>
+      <FlatList
+        data={drivers}
+        renderItem={({ item }) => (
+          <DriverCard
+            selected={selectedDriver!}
+            setSelected={() => setSelectedDriver(Number(item.id)!)}
+            item={item}
+          />
+        )}
+        ListFooterComponent={() => (
+          <View className="mx-5 mt-10">
+            <CustomButton
+              title="Select Ride"
+              onPress={() => router.push("/(root)/book-ride")}
+            />
+          </View>
+        )}
+      />
+    </RideLayout>
+  );
+};
+
+export default ConfirmRide;
