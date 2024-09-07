e59c1c174e52aff00b87cf261508e12549107882
Prem Desai <121574508+PREMDESAI@users.noreply.github.com>
2024-09-08 01:45:21 +0530

    Add files via upload

---
+++ react-color-generator-project-main/precache-manifest.fead64bcca19b0b9bf61bebb1b8cf522.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ebc008ac48a197acae8d68644a8bbce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "216a56d60140c850876f",
+    "url": "/static/css/main.f6e8d1ab.chunk.css"
+  },
+  {
+    "revision": "dc54179153e91c1bc9ed",
+    "url": "/static/js/2.07aa666e.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/static/js/2.07aa666e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "216a56d60140c850876f",
+    "url": "/static/js/main.bc3a4bf2.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
